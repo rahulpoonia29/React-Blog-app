eb81788a353b6aaeae881abf082a6ead953aa6f0
Rahul <chattychidiya@gmail.com>
2024-05-23 21:13:09 +0530

    Upload image functionality

--- src/pages/NewPost.jsx
+++ src/pages/NewPost.jsx
@@ -6,12 +6,24 @@ import { Controller, useForm } from "react-hook-form";
 import postService from "../appwrite/post";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+import {
+	Select,
+	SelectContent,
+	SelectGroup,
+	SelectItem,
+	SelectLabel,
+	SelectTrigger,
+	SelectValue,
+} from "@/components/ui/select";
+import fileService from "../appwrite/file";
+import { useNavigate } from "react-router-dom";
 
 function NewPost() {
 	const {
 		register,
 		handleSubmit,
 		watch,
+		setValue,
 		formState: { errors },
 		control,
 	} = useForm();
@@ -24,22 +36,35 @@ function NewPost() {
 			.replace(/\s+/g, "-");
 	};
 
+	const navigate = useNavigate();
 	const [slug, setSlug] = useState("");
 	const userData = useSelector((state) => state.auth.userData);
 
 	useEffect(() => {
-		setSlug(slugify(watch("title")));
+		setValue("slug", slugify(watch("title")));
 	}, [watch("title")]);
 
-	const submitPost = (data) => {
-		postService.createPost(slug, { ...data, userID: userData.$id });
+	const submitPost = async (formData) => {
+		// console.log(data.image);
+		fileService.uploadFile(formData.image[0]).then(async (data) => {
+			console.log(data);
+			postService
+				.createPost(formData.slug, {
+					...formData,
+					userID: userData.$id,
+					featuredImage: data.$id,
+				})
+				.then(() => {
+					navigate("/");
+				});
+		});
 	};
 
 	return (
 		<form onSubmit={handleSubmit(submitPost)} noValidate>
 			<div className="flex flex-col gap-4 mx-10 lg:mx-32 my-8">
 				<div className="flex flex-col gap-1.5">
-					<Label className="text-lg" htmlFor="title">
+					<Label className="text-md" htmlFor="title">
 						Title
 					</Label>
 					<div className="flex gap-4">
@@ -51,14 +76,85 @@ function NewPost() {
 								required: "Post title is required",
 							})}
 						/>
-						<Label className="text-">{slug}</Label>
 						<Button type="submit">Create Post</Button>
 					</div>
 					<p className="text-red-500 text-sm">
 						{errors.title && errors.title.message}
 					</p>
 				</div>
+				<div className="flex gap-4">
+					<div>
+						<Label className="text-md" htmlFor="featuredImage">
+							Featured Image
+						</Label>
+						<Input
+							id="featuredImage"
+							type="file"
+							placeholder="Upload the featured image"
+							{...register("image", {
+								required: "Image is required",
+							})}
+						/>
+						<p className="text-red-500 text-sm">
+							{errors.image && errors.image.message}
+						</p>
+					</div>
 
+					<div>
+						<Label className="text-md" htmlFor="status">
+							Status
+						</Label>
+						<Controller
+							name="status"
+							control={control}
+							defaultValue={"active"}
+							rules={{ required: "Status is required" }}
+							render={({ field: { onChange, value } }) => (
+								<Select onValueChange={onChange} value={value}>
+									<SelectTrigger
+										className="max-w-[180px] sm:w-[180px]"
+										id="status"
+									>
+										<SelectValue placeholder="Select post visibility" />
+									</SelectTrigger>
+									<SelectContent>
+										<SelectGroup>
+											<SelectItem value="active">
+												Active
+											</SelectItem>
+											<SelectItem value="inactive">
+												Inactive
+											</SelectItem>
+										</SelectGroup>
+									</SelectContent>
+								</Select>
+							)}
+						/>
+						<p className="text-red-500 text-sm">
+							{errors.status && errors.status.message}
+						</p>
+					</div>
+					<div className="grow">
+						<Label className="text-md" htmlFor="slug">
+							Slug
+						</Label>
+						<Input
+							id="slug"
+							type="text"
+							placeholder="Slug of the post"
+							{...register("slug", {
+								required: "Slug is required",
+								pattern: {
+									value: /^[a-z0-9]+(?:-[a-z0-9]+)*$/,
+									message: "Enter a valid slug",
+								},
+							})}
+						/>
+						<p className="text-red-500 text-sm">
+							{errors.slug && errors.slug.message}
+						</p>
+					</div>
+				</div>
 				<Controller
 					name="content"
 					control={control}
@@ -66,6 +162,7 @@ function NewPost() {
 					render={({ field: { onChange } }) => (
 						<Editor
 							apiKey="9ib8tgwimr5pyqo86th0fbsojkbujylm2m4gvel7q5ycu90u"
+							onEditorChange={onChange}
 							initialValue=""
 							init={{
 								height: 380,
@@ -98,7 +195,6 @@ function NewPost() {
 								content_style:
 									"body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
 							}}
-							onEditorChange={onChange}
 						></Editor>
 					)}
 				/>
