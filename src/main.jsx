adabc50b4930488cf9759397c8c85e1b9423bb68
Rahul <chattychidiya@gmail.com>
2024-05-30 12:18:56 +0530

    feat: Add ThemeProvider for managing app themes

--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,7 @@ import {
 	QueryClient,
 	QueryClientProvider,
 } from "@tanstack/react-query";
+import { ThemeProvider } from "./context/Theme.jsx";
 
 const queryClient = new QueryClient();
 
@@ -18,7 +19,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<Provider store={store}>
 			<QueryClientProvider client={queryClient}>
-				<App />
+				<ThemeProvider>
+					<App />
+				</ThemeProvider>
 			</QueryClientProvider>
 		</Provider>
 	</React.StrictMode>
